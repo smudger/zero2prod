7ab98a229a98138ffb992ff337d59769d669de2d
Louis Smith <15144861+smudger@users.noreply.github.com>
2024-06-28 21:39:27 +0100

    Cargo fix

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,7 +1,6 @@
 use actix_web::{web, HttpResponse, Responder};
 use chrono::Utc;
 use sqlx::PgPool;
-use unicode_segmentation::UnicodeSegmentation;
 use uuid::Uuid;
 
 use crate::domain::{NewSubscriber, SubscriberEmail, SubscriberName};
